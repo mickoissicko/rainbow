b8be545df5e4a936eb2366cd1f585358ce03b6c6
mickoissicko <defualtusermick@gmail.com>
2024-02-26 14:58:07 +0100

    add headers

---
+++ languages/app.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdio.h>
+
+#ifndef APP_H
+ #define APP_H
+
+void World()
+{
+    printf("Hello world\n");
+}
+
+#endif
---
+++ languages/app.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+
+#ifndef APP_H
+ #define APP_H
+
+void World()
+{
+    std::cout << "Hello world" << std::endl;
+}
+
+#endif
